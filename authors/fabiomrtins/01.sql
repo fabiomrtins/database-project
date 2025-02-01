e7bbd8caa56b0721dd95e9e8a9debb56ddedbb3d
Aluno Lab 04 <aluno.lab04@ifpe.edu>
2025-02-01 11:32:30 -0300

    ajuste do exercicio

--- authors/fabiomrtins/01.sql
+++ authors/fabiomrtins/01.sql
@@ -1,1 +1,9 @@
+-- 1. Liste os produtos.
 SELECT * FROM products;
+-- 1. Liste os produtos e o nome de seus fornecedores.
+-- 2. Recupere os pedidos com os nomes dos clientes e dos funcionários responsáveis.
+-- 3. Liste os produtos, suas categorias e o nome dos fornecedores.
+-- 4. Quantos pedidos cada cliente já fez?
+-- 5. Valor total vendido por cada funcionário (considerando os pedidos já enviados).
+-- 6. Média de preços dos produtos por categoria.
+-- 7. Quais os clientes que nunca fizeram pedidos?
