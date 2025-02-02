988a258a8c3e46a3edc777a334861e9ec812975d
raffael-marinho <silvamarinho10@gmail.com>
2025-02-02 13:18:05 -0300

    add lista de produtos e o nome dos contatos dos fornecedores

--- authors/fabiomrtins/01.sql
+++ authors/fabiomrtins/01.sql
@@ -1,6 +1,17 @@
 -- 1. Liste os produtos.
-SELECT * FROM products;
+SELECT
+    *
+FROM
+    products;
+
 -- 1. Liste os produtos e o nome de seus fornecedores.
+SELECT
+    products.product_name,
+    suppliers.contact_name
+FROM
+    products
+    JOIN suppliers ON products.supplier_id = suppliers.supplier_id;
+
 -- 2. Recupere os pedidos com os nomes dos clientes e dos funcionários responsáveis.
 -- 3. Liste os produtos, suas categorias e o nome dos fornecedores.
 -- 4. Quantos pedidos cada cliente já fez?
