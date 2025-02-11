9dcf3bc3793c6972e86956800f6b14a40b43981c
Guilherme Valença Rodrigues Pereira <56165820+Guilhermevalenca@users.noreply.github.com>
2025-02-11 15:34:59 -0300

    Update 03.sql

--- authors/Guilhermevalenca/03.sql
+++ authors/Guilhermevalenca/03.sql
@@ -5,6 +5,6 @@ select
 from 
 	orders
 left join
-	customers on orders.customer_id = customers.customers.id
+	customers on orders.customer_id = customers.customer.id
 left join
-	employees on orders.employee_id = employees.employees.id;
+	employees on orders.employee_id = employees.employee.id;
