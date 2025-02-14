8102951a022ec44f71e88b9f25e9c486c5668542
Felipe Romero Alves da Silva <fras2@discente.ifpe.edu.br>
2025-02-14 13:44:14 -0300

    feat: exercicio 5

--- authors/feliperasilva/feliperomero04.sql
+++ authors/feliperasilva/feliperomero04.sql
@@ -1,4 +1,4 @@
 SELECT products.product_id, products.product_name, categories.category_name, suppliers.company_name
 FROM products
-JOIN categories ON products.category_id = categories.category_id
-JOIN suppliers ON products.supplier_id = suppliers.supplier_id;
+INNER JOIN categories ON products.category_id = categories.category_id
+INNER JOIN suppliers ON products.supplier_id = suppliers.supplier_id;
